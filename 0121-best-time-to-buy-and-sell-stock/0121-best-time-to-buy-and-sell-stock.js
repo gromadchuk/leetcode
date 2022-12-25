f662e9c1654bdaca16ec027ac7a80d0f2fc34555
Pavel Gromadchuk <gromadchuk.pasha@gmail.com>
2022-12-26 02:14:28 +0300

    Time: 99 ms (77.43%), Space: 52 MB (16.31%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    if (prices.length < 2) {
+        return 0;
+    }
+
+    let currentPrice = prices[0];
+    let result = 0;
+
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] > currentPrice && (prices[i] - currentPrice) > result) {
+            result = prices[i] - currentPrice;
+        }
+
+        if (prices[i] < currentPrice) {
+            currentPrice = prices[i];
+        }
+    }
+
+    return result;
+};
