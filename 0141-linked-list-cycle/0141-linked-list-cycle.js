f2740bcdd41620ab53f03c83b7ee6ee8b594bcc5
Pavel Gromadchuk <gromadchuk.pasha@gmail.com>
2022-12-26 02:13:57 +0300

    Time: 66 ms (99.20%), Space: 45 MB (46.76%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    while (head) {
+        if (head.linked) {
+            return true;
+        }
+
+        head.linked = true;
+        head = head.next;
+    }
+
+    return false;
+};
