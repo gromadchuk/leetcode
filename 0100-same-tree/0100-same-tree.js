891535cb60813b1f5b6539bf4c133f298691ab9e
Pavel Gromadchuk <gromadchuk.pasha@gmail.com>
2022-12-26 02:15:02 +0300

    Time: 74 ms (71.89%), Space: 42.5 MB (35.20%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.js
@@ -0,0 +1,41 @@
+const getDeepInfo = (node) => {
+    const result = [];
+
+    if (node) {
+        result.push(node.val);
+
+        if (node.left || node.right) {
+            if (node.left) {
+                result.push(...getDeepInfo(node.left));
+            } else {
+                result.push(null);
+            }
+
+            if (node.right) {
+                result.push(...getDeepInfo(node.right));
+            }
+        }
+    }
+
+    return result;
+};
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+const isSameTree = function(p, q) {
+    const firstNode = getDeepInfo(p);
+    const secondNode = getDeepInfo(q);
+
+    return firstNode.join() === secondNode.join();
+};
