b03e40da3d8df8d88c6cfd7884958b2764df9eb1
Pavel Gromadchuk <gromadchuk.pasha@gmail.com>
2022-12-26 02:06:09 +0300

    Time: 137 ms (71.23%), Space: 52.1 MB (41.94%) - LeetHub

---
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} allowed
+ * @param {string[]} words
+ * @return {number}
+ */
+var countConsistentStrings = function(allowed, words) {
+    allowed = allowed.split('');
+    
+    const filtered = words.filter((word) => {
+        for (let i = 0; i < word.length; i++) {
+            if (!allowed.includes(word[i])) {
+                return false;
+            }
+        }
+        
+        return true;
+    });
+    
+    return filtered.length;
+};
