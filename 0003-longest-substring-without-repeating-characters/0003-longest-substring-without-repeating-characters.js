8fb4f7a60e2d2657f14fdaf939fd7cfb4827bd0e
Pavel Gromadchuk <gromadchuk.pasha@gmail.com>
2022-12-27 15:52:17 +0300

    Time: 79 ms (97.93%), Space: 44.7 MB (95.31%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -2,7 +2,7 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
+const lengthOfLongestSubstring = function(s) {
     if (s.length < 2) {
         return s.length;
     }
