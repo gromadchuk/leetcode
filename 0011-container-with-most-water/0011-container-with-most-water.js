cd349eef8c90dfece1e7ae9aa443301d1a680d08
Pavel Gromadchuk <gromadchuk.pasha@gmail.com>
2022-12-27 20:01:05 +0300

    Time: 76 ms (95.05%), Space: 48.5 MB (99.27%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    if (height.length === 2) {
+        return Math.min(...height);
+    }
+    
+    let result = 0;
+    let i = 0;
+    let j = height.length - 1;
+    
+    while (i < j) {
+        const water = Math.min(height[i], height[j]) * (j - i);
+        
+        result = Math.max(result, water);
+        
+        if (height[i] <= height[j]) {
+            i++;
+        } else {
+            j--;
+        }
+    }
+    
+    return result;
+};
