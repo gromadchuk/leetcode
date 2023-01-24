1268fb4a4b29f4c1b2668b1b8ff0285e5b3ef0c7
Pavel Gromadchuk <gromadchuk.pasha@gmail.com>
2023-01-24 19:58:42 +0300

    Time: 698 ms (9.86%), Space: 48.8 MB (11.16%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -2,7 +2,7 @@
  * @param {number[]} nums
  * @return {number}
  */
-var singleNumber = function(nums) {
+const singleNumber = function(nums) {
     for (let num of nums) {
         const find = nums.filter(itNum => itNum === num);
 
